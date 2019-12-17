ca7ff693048675ca9f7a0feeefab9ffc372a220f
Diego Rodrigues <diegurm@hotmail.com>
2019-12-17 09:23:34 -0300

    Criado ação para remover tecnologia ja cadastrada

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,16 +1,47 @@
 import React, { Component } from "react";
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "React.js", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
   };
 
   render() {
     return (
-      <ul>
-        {this.state.techs.map(tech => (
-          <li>{tech}</li>
-        ))}
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={this.state.techs.indexOf(tech)}>
+              {tech}
+              <button type="button" onClick={() => this.handleDelete}>
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
